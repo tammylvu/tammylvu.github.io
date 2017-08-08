21b9d0d65f558c4e95077d7ea766908e1dad45c9
Dang-Nam <dangnamvu@outlook.com>
2017-08-08 00:14:24 -0400

    scroll events

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,10 +1,14 @@
 $(window).scroll(function() {
-   var hT = $('#scroll-to').offset().top,
-       hH = $('#scroll-to').outerHeight(),
-       wH = $(window).height(),
-       wS = $(this).scrollTop();
-       
-   if (wS > (hT+hH-wH)){
-       console.log('H1 on the view!');
-   }
+	var hT = $(".about").offset().top,
+	hH = $(".about").outerHeight(),
+	wH = $(window).height(),
+	wS = $(this).scrollTop();
+
+	if (wS > (hT+hH-wH)){
+		$(".about").addClass("scrolled");
+	}
+
+	else {
+		$(".about").removeClass("scrolled");
+	}
 });
