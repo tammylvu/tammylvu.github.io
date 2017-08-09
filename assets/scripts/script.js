b826b94e22e4a5b4591ee50e6aeda8a23894d0b8
Dang-Nam <dangnamvu@outlook.com>
2017-08-09 00:29:00 -0400

    i don't fucking get it

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,14 +1,34 @@
-$(window).scroll(function() {
-	var hT = $(".about").offset().top,
-	hH = $(".about").outerHeight(),
-	wH = $(window).height(),
-	wS = $(this).scrollTop();
+// $(window).scroll(function() {
+// 	var hT = $(".about").offset().top,
+// 	hH = $(".about").outerHeight(),
+// 	wH = $(window).height(),
+// 	wS = $(this).scrollTop();
 
-	if (wS > (hT+hH-wH)){
+// 	if (wS > (hT+hH-wH)){
+// 		$(".about").addClass("scrolled");
+// 	}
+
+// 	else {
+// 		$(".about").removeClass("scrolled");
+// 	}
+// });
+
+$(window).scroll(function() {
+	if ($(window).scrollTop() >= $(".about").position().top) {
 		$(".about").addClass("scrolled");
 	}
 
-	else {
-		$(".about").removeClass("scrolled");
+	else if ($(window).scrollTop() >= $(".projects").position().top) {
+		$(".projects").addClass("scrolled");
 	}
+
+	else if ($(window).scrollTop() >= $(".resume").position().top) {
+		$(".resume").addClass("scrolled");
+	}
+	else {
+		$(".container").removeClass("scrolled");
+	}
+
+	console.log($(window).scrollTop());
+	console.log($(".projects").position().top);
 });
