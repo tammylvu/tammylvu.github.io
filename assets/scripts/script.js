7f2cd25a443f14716b8756dc4e7c2970b71b25f6
Dangnam <dangnam.vu@pefin.com>
2017-08-10 17:51:36 -0400

    scrolling stuff

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,6 +2,10 @@ $(document).ready(function () {
 	$(".current-section-container").click(function () {
 		$("nav, .overlay").toggleClass("open");
 	});
+
+	$(".overlay").click(function () {
+		$("nav, .overlay").removeClass("open");
+	});
 });
 
 
@@ -11,35 +15,63 @@ let logged = false;
 $(window).scroll(function() {
 	let windowT = $(window).scrollTop();
 
-	let firstT = $(".first-section").position().top;
-	let firstH = firstT + $(".first-section").outerHeight();
+	let firstT = $(".container.first-section").position().top;
+	let firstH = firstT + $(".container.first-section").outerHeight();
 
-	let aboutT = $(".about").position().top;
-	let aboutH = aboutT + $(".about").outerHeight();
+	let aboutT = $(".container.about").position().top;
+	let aboutH = aboutT + $(".container.about").outerHeight();
 
-	let projectsT = $(".projects").position().top;
-	let projectsH = projectsT + $(".projects").outerHeight();
+	let projectsT = $(".container.projects").position().top;
+	let projectsH = projectsT + $(".container.projects").outerHeight();
 
-	let resumeT = $(".resume").position().top;
-	let resumeH = resumeT + $(".resume").outerHeight();
+	let resumeT = $(".container.resume").position().top;
+	let resumeH = resumeT + $(".container.resume").outerHeight();
+
+	let contactT = $(".container.contact").position().top;
+	let contactH = resumeT + $(".container.contact").outerHeight();
 
 	if (windowT >= firstT) {
 		$("nav").addClass("scrolled");
 	}
 
-	if (windowT >= aboutT && windowT < aboutH) {
+	else if (windowT >= firstT && windowT < firstH) {
+		$("nav").removeClass();
 		$(".container").removeClass("active");
+
+		$(".container.first-section").addClass("active");
+		$("nav.welcome").addClass("welcome");
+	}
+
+	else if (windowT >= aboutT && windowT < aboutH) {
+		$("nav").removeClass();
+		$(".container").removeClass("active");
+
 		$(".container.about").addClass("active");
+		$("nav.about").addClass("about");
 	}
 
 	else if (windowT >= projectsT && windowT < projectsH) {
+		$("nav").removeClass();
 		$(".container").removeClass("active");
+
 		$(".container.projects").addClass("active");
+		$("nav.projects").addClass("projects");
 	}
 
 	else if (windowT >= resumeT && resumeH) {
+		$("nav").removeClass();
 		$(".container").removeClass("active");
+
 		$(".container.resume").addClass("active");
+		$("nav.resume").addClass("resume");
+	}
+
+	else if (windowT >= contactT && contactH) {
+		$("nav").removeClass();
+		$(".container").removeClass("active");
+
+		$(".container.contact").addClass("active");
+		$("nav.contact").addClass("contact");
 	}
 
 	else {
