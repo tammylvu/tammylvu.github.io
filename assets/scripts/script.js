1a961dd67882eb051aac226ae28f54e17a9063c6
Dangnam <dangnam.vu@pefin.com>
2017-08-21 17:23:11 -0400

    hide on scroll test

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -38,12 +38,6 @@ $(window).scroll(function() {
 		$(".container").removeClass("active");
 	}
 
-	// if (windowT >= firstT) {
-	// 	reset();
-
-	// 	$("nav").addClass("scrolled");
-	// }
-
 	if (windowT >= firstT && windowT < firstH) {
 		reset();
 
@@ -108,3 +102,43 @@ $(window).scroll(function() {
 	// console.log("windowT is " + windowT);
 
 });
+
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navHeight){
+        // Scroll Down
+        $('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+
+    lastScrollTop = st;
+}
